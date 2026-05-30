dc779d940d8b383ca7429b51539c16cdf4a4b0fa
Maciej <macus450@gmail.com>
2026-05-30 21:50:22 +0200

    feat: add environment-based API configuration

---
+++ ioc_triage/config.py
@@ -0,0 +1,8 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+VIRUSTOTAL_API_KEY = os.getenv("VIRUSTOTAL_API_KEY")
+ABUSEIPDB_API_KEY = os.getenv("ABUSEIPDB_API_KEY")
+REQUEST_TIMEOUT = int(os.getenv("REQUEST_TIMEOUT"))
